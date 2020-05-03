0b093b8bf69e868830ff563346877c08b2b25370
zhaodan <zhaodan@limikeji.com>
2020-05-03 08:08:28 +0800

    KEEP-ACTIVE

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
   <div id="app" class="g-container">
     <!--页面展示区，只有这里可以滚动-->
     <div class="g-view-container">
-      <router-view/>
+      <!--keep-alive保存在内存中，tab切换时-->
+      <keep-alive  v-if="$route.meta.keepAlive">
+        <router-view/>
+      </keep-alive>
     </div>
     <div class="g-footer-container">
       <Tabbar/>
--- src/router/index.js
+++ src/router/index.js
@@ -6,27 +6,42 @@ const routes = [
   {
     name: 'home',
     path: '/home',
-    component: () => import('pages/home')
+    component: () => import('pages/home'),
+    meta: {
+      keepAlive: true // 需要被缓存
+    }
   },
   {
     name: 'cart',
     path: '/cart',
-    component: () => import('pages/cart')
+    component: () => import('pages/cart'),
+    meta: {
+      keepAlive: true // 需要被缓存
+    }
   },
   {
     name: 'category',
     path: '/category',
-    component: () => import('pages/category')
+    component: () => import('pages/category'),
+    meta: {
+      keepAlive: true // 需要被缓存
+    }
   },
   {
     name: 'personal',
     path: '/personal',
-    component: () => import('pages/personal')
+    component: () => import('pages/personal'),
+    meta: {
+      keepAlive: true // 需要被缓存
+    }
   },
   {
     name: 'search',
     path: '/search',
-    component: () => import('pages/search')
+    component: () => import('pages/search'),
+    meta: {
+      keepAlive: true // 需要被缓存
+    }
   },
   // 匹配不到地址跳到home页
   {
